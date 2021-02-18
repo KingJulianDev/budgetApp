4226f927173cdec00f0e22580abadcea614ca461
KingJulianDev <kingurianjulian@gmail.com>
2021-02-19 00:46:51 +0200

    commit

--- scripts.js
+++ scripts.js
@@ -12,65 +12,96 @@ let currentBalance = 0
 let historyArr = []
 let idOfActivities = 0
 
-function createHistoryItem(el, color){
-    historyList.insertAdjacentHTML(
-        'beforebegin',
-        `
-            <li>
-                <div class="history-label ${color}">${el.name}</div>
+function createHistoryItem(el) {
+  ul.insertAdjacentHTML(
+    'afterbegin',
+    `<li class="${el.type} history-item" id="${el.id}">
+      <div class="history-label ${el.color}">${el.name}</div>
 
-                <div class="history-value">$${el.value}</div>
+      <div class="history-value">$${el.value}</div>
 
-                <div class="li-item-options">
-                    <div class="item-option edit-history-item">X</div>
-                    <div class="item-option delete-history-item">?</div>
-                </div>
-            </li>
-        `
-    )
+      <div class="li-item-options">
+          <div class="item-option edit-history-item">X</div>
+          <div class="item-option delete-history-item">?</div>
+      </div>
+    </li>`
+  )
 }
 
 addIncome.onclick = () => {
-    (currentBudget === 0) ? currentBudget = Number(plusInputValue.value) : currentBudget += Number(plusInputValue.value);
-    document.querySelector('.budget').innerHTML = currentBudget
-    currentBalance = currentBudget - currentExpenses
-    document.querySelector('.balance').innerHTML = currentBalance
-    let historyItem = {
-        id: idOfActivities,
-        type: 'income',
-        name: plusInputName.value,
-        value: Number(plusInputValue.value)
-    }
-    historyArr.push(historyItem)
-    createHistoryItem(historyItem, 'green')
-    idOfActivities ++
-    plusInputValue.value = ''
-    plusInputName.value = ''
-    console.log(historyArr);
-    
+  currentBudget === 0
+    ? (currentBudget = Number(plusInputValue.value))
+    : (currentBudget += Number(plusInputValue.value))
+  document.querySelector('.budget').innerHTML = currentBudget
+  currentBalance = currentBudget - currentExpenses
+  document.querySelector('.balance').innerHTML = currentBalance
+  let historyItem = {
+    id: idOfActivities,
+    type: 'income',
+    name: plusInputName.value,
+    value: Number(plusInputValue.value),
+    color: 'green',
+  }
+  historyArr.push(historyItem)
+  createHistoryItem(historyItem, 'green', 'income')
+  idOfActivities++
+  plusInputValue.value = ''
+  plusInputName.value = ''
+  console.log(historyArr)
 }
 
 addExpense.onclick = () => {
-    (currentExpenses === 0) ? currentExpenses = Number(minusInputValue.value) : currentExpenses += Number(minusInputValue.value);
-    document.querySelector('.expenses').innerHTML = currentExpenses
-    currentBalance = currentBudget - currentExpenses
-    document.querySelector('.balance').innerHTML = currentBalance
-    let historyItem = {
-        id: idOfActivities,
-        type: 'expense',
-        name: minusInputName.value,
-        value: Number(minusInputValue.value)
-    }
-    historyArr.push(historyItem)
-    createHistoryItem(historyItem, 'red')
-    idOfActivities ++
-    minusInputValue.value = ''
-    minusInputName.value = ''
-    console.log(historyArr);  
+  currentExpenses === 0
+    ? (currentExpenses = Number(minusInputValue.value))
+    : (currentExpenses += Number(minusInputValue.value))
+  document.querySelector('.expenses').innerHTML = currentExpenses
+  currentBalance = currentBudget - currentExpenses
+  document.querySelector('.balance').innerHTML = currentBalance
+  let historyItem = {
+    id: idOfActivities,
+    type: 'expense',
+    name: minusInputName.value,
+    value: Number(minusInputValue.value),
+    color: 'red',
+  }
+  historyArr.push(historyItem)
+  createHistoryItem(historyItem, 'red', 'expense')
+  idOfActivities++
+  minusInputValue.value = ''
+  minusInputName.value = ''
+  console.log(historyArr)
 }
 
 let test = document.getElementById('btn')
 test.onclick = () => {
-    let kek = historyArr.filter(el => el.type === 'income')
-    console.log(kek)
+  console.log(historyArr)
+  let liList = Array.from(document.querySelectorAll('.history-item'))
+  console.log(Array.from(liList))
+  console.log(typeof liList)
+}
+
+const ul = document.querySelector('.ul')
+const clear = document.querySelector('.clear-btn')
+clear.onclick = () => {
+  ul.innerHTML = ''
+}
+
+function sortHistoryItems(param) {
+  let sortedItems = historyArr.filter((el) => el.type === param)
+  ul.innerHTML = ''
+  console.log(sortedItems)
+  sortedItems.forEach((el) => {
+    createHistoryItem(el)
+  })
+}
+
+const sortIncome = document.querySelector('.sortIncome')
+const sortExpense = document.querySelector('.sortExpense')
+
+sortIncome.onclick = () => {
+  sortHistoryItems('income')
+}
+
+sortExpense.onclick = () => {
+  sortHistoryItems('expense')
 }
